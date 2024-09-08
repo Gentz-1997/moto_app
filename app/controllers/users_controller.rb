e57c611220408bff595fb29a5425d3a33a512076
Matsumi Genki <genki.kuromu@gmail.com>
2024-09-08 23:45:14 +0900

    コントローラにcreateを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      flash[:notice] = "登録しました"
+      redirect_to user_path(@user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :crypted_password)
+  end
 end
