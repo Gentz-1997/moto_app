5939c04ddbc7514014b0de7dba5b8ad3cfe1ffa2
Matsumi Genki <genki.kuromu@gmail.com>
2024-09-12 11:09:33 +0900

    add-devise-views

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, {presence: :true}
   validates :email, {presence: :true}
-  validates :crypted_password, {presence: :true}
+  validates :encrypted_password, {presence: :true}
 end
