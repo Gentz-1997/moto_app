dccd1b99f8d32615c5dbe7e1367adb88dafdcb92
Matsumi Genki <genki.kuromu@gmail.com>
2024-09-21 18:17:18 +0900

    ヘッダーメニューの条件分岐を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,25 @@
 class UsersController < ApplicationController
-  # def show
-  #   # @user = User.find(params[:id])
+  def show
+    @user = User.find_by(id: params[:id]) || current_user
+  end
+
+  # def new
+  #   @user = User.new
   # end
 
-  def new
-    @user = User.new
-  end
+  # def create
+  #   @user = User.create(user_params)
+  #   if @user.save
+  #     flash[:notice] = "登録しました"
+  #     redirect_to user_path(@user)
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
 
-  def create
-    @user = User.create(user_params)
-    if @user.save
-      flash[:notice] = "登録しました"
-      redirect_to user_path(@user)
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+  # private
 
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :crypted_password)
-  end
+  # def user_params
+  #   params.require(:user).permit(:name, :email, :crypted_password)
+  # end
 end
