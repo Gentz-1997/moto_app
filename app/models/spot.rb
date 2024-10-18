a53b5e6bc30e9aff69164993a802de62fd789d25
Matsumi Genki <genki.kuromu@gmail.com>
2024-10-18 11:55:21 +0900

    spot_name,addressにvalidationを追加

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -5,6 +5,9 @@ class Spot < ApplicationRecord
 
   accepts_nested_attributes_for :spot_tags, allow_destroy: true
 
+  validates :spot_name, presence: true, length: {maximum: 255}
+  validates :address, presence: true
+
   geocoded_by :address
   after_validation :geocode
 end
