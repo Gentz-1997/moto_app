1a9d527a15630dc946c250932c6816ba0b72b616
Matsumi Genki <genki.kuromu@gmail.com>
2024-10-19 12:40:22 +0900

    ユーザー編集後のページを変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,24 +5,4 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @spots = @user.spots
   end
-
-  # def new
-  #   @user = User.new
-  # end
-
-  # def create
-  #   @user = User.create(user_params)
-  #   if @user.save
-  #     flash[:notice] = "登録しました"
-  #     redirect_to user_path(@user)
-  #   else
-  #     render :new, status: :unprocessable_entity
-  #   end
-  # end
-
-  # private
-
-  # def user_params
-  #   params.require(:user).permit(:name, :email, :crypted_password)
-  # end
 end
