6b84e78cd3b09154a9ca0e02235a71ea8c5c7426
Matsumi Genki <genki.kuromu@gmail.com>
2024-10-20 12:28:48 +0900

    usernameにvalidationを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :spots, dependent: :destroy
   has_one_attached :avatar
+  validates :name, presence: true, length: {maximum: 50}
   validates :avatar,
   content_type: {in: %w[image/jpeg image/gif image/png], message: "は有効な形式のファイルをファイルを選択して下さい。" },
   size:{ less_than: 5.megabytes,message:"5MB以下のファイルを選択して下さい。"}
