60485dd1630f0277f2e0017e1335a9d1feb63a85
Matsumi Genki <genki.kuromu@gmail.com>
2024-10-27 13:27:50 +0900

    usersにcorrect_userを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,21 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user
+  before_action :correct_user
 
   def show
-    @user = User.find_by(id: params[:id])
     @spots = @user.spots
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+  end
+
+  def correct_user
+    if @user != current_user
+      redirect_to root_path, alert: "権限がありません"
+    end
   end
 end
