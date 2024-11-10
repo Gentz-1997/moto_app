a7fc660a20f7977a73b221f86ed25b107cf4f4e4
Matsumi Genki <genki.kuromu@gmail.com>
2024-11-11 01:20:23 +0900

    UI改善

--- app/javascript/files/tabs.js
+++ app/javascript/files/tabs.js
@@ -1,10 +1,23 @@
 'use strict';
-export function openTab(tabId) {
-  const tabs = document.querySelectorAll('[data-tab-content]');
-  tabs.forEach(tab => tab.classList.add('hidden'));
+export default function setupTabs() {
+  function openTab(tabId) {
+    const tabs = document.querySelectorAll('[data-tab-content]');
+    tabs.forEach(tab => tab.classList.add('hidden'));
 
-  const targetTab = document.getElementById(tabId);
-  if (targetTab) targetTab.classList.remove('hidden');
+    const targetTab = document.getElementById(tabId);
+    if (targetTab) targetTab.classList.remove('hidden');
+  }
+
+  document.querySelectorAll('[data-tab-target]').forEach(button => {
+    button.addEventListener('click', () => {
+      const tabId = button.getAttribute('data-tab-target');
+      openTab(tabId);
+    });
+  });
+
+  // Turboのページ遷移後に初期タブを設定
+  document.addEventListener('turbo:load', () => {
+    openTab('spots');
+    setupTabs();
+  });
 }
-
-window.openTab = openTab;
